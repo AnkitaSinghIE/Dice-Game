944cfc607bd92bfca7a6825ae6ae52d6937efa13
AnkitaSinghIE <ankita.singh.200020@gmail.com>
2021-06-16 11:58:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var randomNumber1=Math.random();
+randomNumber1=randomNumber1*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+
+var randomDiceImage1="dice" + randomNumber1 + ".png";
+
+var randomImageSource1="images/"+randomDiceImage1;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource1);
+
+
+var randomNumber2=Math.random();
+randomNumber2=randomNumber2*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="player 1 won";
+}
+else if(randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML="player 2 won";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
